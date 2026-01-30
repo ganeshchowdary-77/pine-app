de27ac43fa1959c85bac8efca8ee175b19869c20
Ganesh Chowdary <infinityrunner7@gmail.com>
2026-01-30 16:07:06 +0530

    Added lazy loading routing for auth, trainer, admin modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: 'auth',
+      loadChildren: () =>
+        import('./auth/auth-module').then(m => m.AuthModule)
+    },
+    {
+      path: 'admin',
+      loadChildren: () =>
+        import('./admin/admin-module').then(m => m.AdminModule)
+    },
+    {
+      path: 'trainer',
+      loadChildren: () =>
+        import('./trainer/trainer-module').then(m => m.TrainerModule)
+    },
+    {
+      path: '',
+      redirectTo: 'auth',
+      pathMatch: 'full'
+    }
+];
